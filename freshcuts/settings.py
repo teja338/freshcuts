f8b6363f8cde1c126a8856aa1dfcb36241826bbd
teja338 <tejchilumula@gmail.com>
2026-07-30 23:25:17 +0530

    Remove STATICFILES_STORAGE setting
    Removed Whitenoise storage configuration for static files.

--- freshcuts/settings.py
+++ freshcuts/settings.py
@@ -90,7 +90,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STORAGES = {
