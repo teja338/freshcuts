2c70a8517ffc699a140af7d8760a4b516a884620
teja338 <tejchilumula@gmail.com>
2026-07-30 23:06:57 +0530

    Update settings.py

--- freshcuts/settings.py
+++ freshcuts/settings.py
@@ -89,7 +89,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
