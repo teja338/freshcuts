46846c36b8b1977d8bca6b47808690db61694561
teja338 <tejchilumula@email.com>
2026-08-06 04:24:06 +0530

    Production settings

--- freshcuts/settings.py
+++ freshcuts/settings.py
@@ -10,9 +10,12 @@ SECRET_KEY = os.environ.get(
     "django-insecure-local-development-secret-key-123456789"
 )
 
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+ALLOWED_HOSTS = [
+    "freshcuts-vn5g.onrender.com",
+    "127.0.0.1",
+    "localhost",
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
